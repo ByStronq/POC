f11b0fe696466c7ad5c569ff4bee02197d1efbc7
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-12-28 01:21:12 +0300

    IoC container definitions were created for the classes that were implemented from the IService and IDal interfaces with Autofac, and reverse mapping definitions were created for the dtos that were implemented from the IDto interface of the classes that were implemented from the IEntity interfaces with AutoMapper. IoC container definitions in AutofacBusinessModule and automapper are added to WebAPI as middleware.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<EmployeeManager>().As<IEmployeeService>().SingleInstance();
+            builder.RegisterType<EfEmployeeDal>().As<IEmployeeDal>().SingleInstance();
+
+            builder.RegisterType<DepartmentManager>().As<IDepartmentService>().SingleInstance();
+            builder.RegisterType<EfDepartmentDal>().As<IDepartmentDal>().SingleInstance();
+
+            builder.RegisterType<DepartmentAssignmentManager>().As<IDepartmentAssignmentService>();
+            builder.RegisterType<EfDepartmentAssignmentDal>().As<IDepartmentAssignmentDal>();
+        }
+    }
+}
---
+++ Business/Mappings/AutoMapper/GeneralMapping.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.Mappings.AutoMapper
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Employee, EmployeeDto>().ReverseMap();
+            CreateMap<Department, DepartmentDto>().ReverseMap();
+            CreateMap<DepartmentAssignment, DepartmentAssignmentDto>().ReverseMap();
+
+            CreateMap<Employee, EmployeeCreateDto>().ReverseMap();
+            CreateMap<Department, DepartmentCreateDto>().ReverseMap();
+            CreateMap<DepartmentAssignment, DepartmentAssignmentCreateDto>().ReverseMap();
+
+            CreateMap<Employee, EmployeeUpdateDto>().ReverseMap();
+            CreateMap<Department, DepartmentUpdateDto>().ReverseMap();
+            CreateMap<DepartmentAssignment, DepartmentAssignmentUpdateDto>().ReverseMap();
+
+            CreateMap<Employee, EmployeeDetailDto>().ReverseMap();
+            CreateMap<Department, DepartmentDetailDto>().ReverseMap();
+            CreateMap<DepartmentAssignment, DepartmentAssignmentDetailDto>().ReverseMap();
+        }
+    }
+}
