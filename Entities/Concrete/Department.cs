fd8cfe046a869a566723299a3398031709dabe71
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-12-27 12:41:59 +0300

    Entities created.

---
+++ Entities/Concrete/Department.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Department : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? DepartmentId { get; set; }
+        public virtual Department ParentDepartment { get; set; }
+        public virtual ICollection<Department> SubDepartments { get; set; }
+    }
+}
---
+++ Entities/Concrete/Employee.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Employee : IEntity
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
