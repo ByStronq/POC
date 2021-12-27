92e0cc4f61bc87c05af72d1bc504deb80f974a7c
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-12-27 16:16:06 +0300

    DataAccessLayer (Dal) interfaces that implement from IEntityRepository<> generic interface and EntityFramework DataAccessLayer (Dal) classes that implement from these interfaces and inherit from EfRepositoryBase<TEntity, TContext> class and EfContext inherit from DbContext to TContext classes were created.

---
+++ DataAccess/Concrete/EntityFramework/EfContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=POC;Trusted_Connection=true");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
