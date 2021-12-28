cc1ce7e177f3edf1be5b5efb74c56c5eed41cfeb
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-12-28 14:02:20 +0300

    MVC Controller classes created.

---
+++ WebMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ WebMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
