380fa6d33727fa3dec2304d38da2cb327637d72b
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-12-29 00:38:57 +0300

    An extension static class has been created for the jQuery DataTable under the Business.Extensions namespace, and a DataTablesModel has been created under the Business.Models. Also, some extensions have been written for EntityFramework that should be used in DataTableExtensions.

---
+++ Business/Extensions/DataTableExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Business.Models;
+using Core.Entities;
+
+namespace Business.Extensions
+{
+    public static class DataTableExtensions
+    {
+        public static DatatablesModel.DataTablesResponseParameter<TEntity> List<TEntity>(this DatatablesModel.DataTablesRequestParameter dataTablesRequestParameter, IQueryable<TEntity> queryable)
+            where TEntity : class, IEntity, new()
+        {
+            DatatablesModel.DataTablesResponseParameter<TEntity> dataTablesResponseParameter = new DatatablesModel.DataTablesResponseParameter<TEntity>();
+
+            try
+            {
+                foreach (DatatablesModel.DataTablesRequestParameter.DataTablesRequestColumn ColumnItem in dataTablesRequestParameter.columns)
+                {
+                    bool isEquality = ColumnItem.IsEquality;
+
+                    string data = ColumnItem.data;
+                    string value = ColumnItem.search.value;
+
+                    if (!String.IsNullOrWhiteSpace(value))
+                        queryable = queryable.Where(data, value, isEquality);
+                }
+
+                string[] orderQueryArray = dataTablesRequestParameter.SortOrder.Split(' ');
+                string orderPropertyName = orderQueryArray[0];
+                bool isDesc = orderQueryArray.Length >= 2 && orderQueryArray[1] == "DESC" ? true : false;
+
+                if (!String.IsNullOrWhiteSpace(orderPropertyName))
+                    queryable = queryable.OrderBy(x => 0)
+                        .OrderBy(orderPropertyName, isDesc, isThenBy: true);
+                else queryable = queryable.OrderBy(x => 0);
+
+                List<TEntity> SearchEntityList = queryable.Skip(dataTablesRequestParameter.start).Take(dataTablesRequestParameter.length).ToList();
+
+                dataTablesResponseParameter.draw = dataTablesRequestParameter.draw;
+
+                dataTablesResponseParameter.recordsTotal = queryable.Count();
+                dataTablesResponseParameter.recordsFiltered = dataTablesResponseParameter.recordsTotal;
+
+                dataTablesResponseParameter.data = SearchEntityList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return dataTablesResponseParameter;
+        }
+    }
+}
---
+++ Business/Extensions/EfExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Business.Extensions
+{
+    public static class EfExtensions
+    {
+        public static IOrderedQueryable<TSource> OrderBy<TSource>(
+         this IQueryable<TSource> query, string propertyName, bool isDesc = false, bool isThenBy = false)
+        {
+            string orderByMethodName = $"{(isThenBy ? "Then" : "Order")}By{(isDesc ? "Descending" : null)}";
+
+            Type entityType = typeof(TSource);
+
+            PropertyInfo propertyInfo = entityType.GetProperty(propertyName);
+
+            ParameterExpression arg = Expression.Parameter(entityType, "x");
+            MemberExpression property = Expression.Property(arg, propertyName);
+
+            LambdaExpression selector = Expression.Lambda(property, new ParameterExpression[] { arg });
+
+            Type enumarableType = typeof(Queryable);
+            MethodInfo method = enumarableType.GetMethods()
+                 .Where(m => m.Name == orderByMethodName && m.IsGenericMethodDefinition)
+                 .Where(m =>
+                 {
+                     var parameters = m.GetParameters().ToList();
+                     return parameters.Count == 2;
+                 }).Single();
+
+            MethodInfo genericMethod = method
+                 .MakeGenericMethod(entityType, propertyInfo.PropertyType);
+
+            var newQuery = (IOrderedQueryable<TSource>) genericMethod
+                 .Invoke(genericMethod, new object[] { query, selector });
+            return newQuery;
+        }
+
+        public static IOrderedQueryable<TSource> Where<TSource>(
+         this IQueryable<TSource> query, string propertyName, string propertyValue, bool isEquality = false)
+        {
+            Type entityType = typeof(TSource);
+
+            ParameterExpression arg = Expression.Parameter(entityType, "x");
+            MemberExpression property = Expression.Property(arg, propertyName);
+
+            ConstantExpression constantExpression = Expression.Constant(propertyValue, typeof(string));
+
+            MethodInfo toString = typeof(Object).GetMethod("ToString");
+            MethodInfo contains = typeof(string).GetMethod(isEquality ? "Equals" : "Contains", new[] { typeof(string) });
+
+            MethodCallExpression toStringMethodExp = Expression.Call(property, toString);
+            MethodCallExpression containsMethodExp = Expression.Call(toStringMethodExp, contains, constantExpression);
+
+            LambdaExpression selector = Expression.Lambda(containsMethodExp, arg);
+
+            Type enumarableType = typeof(Queryable);
+            MethodInfo method = enumarableType.GetMethods()
+                 .Where(m => m.Name == "Where" && m.IsGenericMethodDefinition)
+                 .Where(m =>
+                 {
+                     var parameters = m.GetParameters().ToList();
+                     return parameters.Count == 2;
+                 }).FirstOrDefault();
+
+            MethodInfo genericMethod = method
+                 .MakeGenericMethod(entityType);
+
+            var newQuery = (IOrderedQueryable<TSource>) genericMethod
+                 .Invoke(genericMethod, new object[] { query, selector });
+            return newQuery;
+        }
+    }
+}
