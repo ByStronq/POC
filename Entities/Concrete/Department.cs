46d390d0a38a3f20de89330d91b31bec5b4a3a62
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-12-29 23:22:01 +0300

    “A Possible Object Cycle Was Detected Which Is Not Supported” Error Solved!

--- Entities/Concrete/Department.cs
+++ Entities/Concrete/Department.cs
@@ -9,8 +9,8 @@ namespace Entities.Concrete
         public int Id { get; set; }
         [Required, MaxLength(256)] public string Name { get; set; }
         public int? DepartmentId { get; set; }
-        [JsonIgnore] public virtual Department ParentDepartment { get; set; }
-        [JsonIgnore] public virtual ICollection<Department> SubDepartments { get; set; }
+        public virtual Department ParentDepartment { get; set; }
+        public virtual ICollection<Department> SubDepartments { get; set; }
         [Required] public DateTime CreatedTime { get; set; } = DateTime.Now;
         [Required] public bool IsActive { get; set; } = true;
     }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,12 +3,18 @@ using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers.Autofac;
 using Business.Mappings.AutoMapper;
 using Core.Entities;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler
+            = ReferenceHandler.IgnoreCycles;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
